8dca89a7dd6170418e506e69873a275ed8e90f90
Alvaro Garcia <derecerca@gmail.com>
2014-07-20 23:51:21 +0200

    refactor: test structure

--- spec/MinesweeperSpec.js
+++ spec/MinesweeperSpec.js
@@ -5,27 +5,35 @@ describe("minesweeper", function(){
 		sut = new Minesweeper();
 	});
 
-
-	it("should create an object", function(){
+	describe("object creation", function(){
+		it("should create an object", function(){
+		});
 	});
 
-	it("should process a zero-dimension array", function(){
-		expect(sut.sweep([])).toEqual([]);
+	describe("corner cases", function(){
+		it("should process a zero-dimension array", function(){
+			expect(sut.sweep([])).toEqual([]);
+		});
 	});
 
-	it("should process a one-dimensional array with just one bomb", function(){
-		expect(sut.sweep(['*'])).toEqual(['*']);
+	describe("one-dimension", function(){
+
+		it("should process a one-dimensional array with just one bomb", function(){
+			expect(sut.sweep(['*'])).toEqual(['*']);
+		});
+
+		it("should process a one-dimensional array with bombs and empty spaces", function(){
+			expect(sut.sweep(['*.'])).toEqual(['*1']);
+		});
+
+		it("should process a one-dimensional array with ovelapping bomb radius", function(){
+			expect(sut.sweep(['.*.*...'])).toEqual(['1*2*100']);
+		});
+
+		it("should process a one-dimensional array with bombs in contact", function(){
+			expect(sut.sweep(['**'])).toEqual(['**']);
+		});
+	
 	});
 
-	it("should process a one-dimensional array with bombs and empty spaces", function(){
-		expect(sut.sweep(['*.'])).toEqual(['*1']);
-	});
-
-	it("should process a one-dimensional array with ovelapping bomb radius", function(){
-		expect(sut.sweep(['.*.*...'])).toEqual(['1*2*100']);
-	});
-
-	it("should process a one-dimensional array with bombs in contact", function(){
-		expect(sut.sweep(['**'])).toEqual(['**']);
-	});
 });
