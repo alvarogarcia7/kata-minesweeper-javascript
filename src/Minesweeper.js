6fa16e45e4d15ab540081aa516bb4bf595edb98c
Alvaro Garcia <derecerca@gmail.com>
2014-07-20 23:36:40 +0200

    refactor: waste reduction - using for when only if is needed

--- src/Minesweeper.js
+++ src/Minesweeper.js
@@ -3,11 +3,10 @@ var Minesweeper = function(){
 }
 
 Minesweeper.prototype.sweep = function(field) {
-	var i;
-	for(i = 0; i < field.length; i++){
-		field[i] = field[i].replace('.','1');
-		if(field[i] === ("*1*")){
-			field[i] = "*2*";
+	if(field.length>0){
+		field[0] = field[0].replace('.','1');
+		if(field[0] === ("*1*")){
+			field[0] = "*2*";
 		}
 	}
 
