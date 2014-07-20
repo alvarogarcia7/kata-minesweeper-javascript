ba2e696a54ecba625e29923b23dd02bf63f7a528
Alvaro Garcia <derecerca@gmail.com>
2014-07-20 23:28:45 +0200

    green: should process a one-dimensional array with bombs and empty spaces

--- spec/MinesweeperSpec.js
+++ spec/MinesweeperSpec.js
@@ -16,4 +16,8 @@ describe("minesweeper", function(){
 	it("should process a one-dimensional array with just one bomb", function(){
 		expect(sut.sweep(['*'])).toEqual(['*']);
 	});
+
+	it("should process a one-dimensional array with bombs and empty spaces", function(){
+		expect(sut.sweep(['*.'])).toEqual(['*1']);
+	});
 });
--- src/Minesweeper.js
+++ src/Minesweeper.js
@@ -3,5 +3,9 @@ var Minesweeper = function(){
 }
 
 Minesweeper.prototype.sweep = function(field) {
+	var i;
+	for(i = 0; i < field.length; i++){
+		field[i] = field[i].replace('.','1');
+	}
 	return field;
 };
