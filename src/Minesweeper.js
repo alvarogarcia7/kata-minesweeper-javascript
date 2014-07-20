3a2ea9a081f2a77de3ac197ceb089ac927330492
Alvaro Garcia <derecerca@gmail.com>
2014-07-20 23:37:59 +0200

    refactor: prefer returning early & multiple times

--- src/Minesweeper.js
+++ src/Minesweeper.js
@@ -3,11 +3,13 @@ var Minesweeper = function(){
 }
 
 Minesweeper.prototype.sweep = function(field) {
-	if(field.length>0){
-		field[0] = field[0].replace('.','1');
-		if(field[0] === ("*1*")){
-			field[0] = "*2*";
-		}
+	if(!field || field.length === 0){
+		return field;
+	}
+
+	field[0] = field[0].replace('.','1');
+	if(field[0] === ("*1*")){
+		field[0] = "*2*";
 	}
 
 	return field;
